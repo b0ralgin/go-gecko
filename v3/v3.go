7bf335880a3bb8a37c339caef4f265983591cf15
Kirill Borodulin <kirill.borodulin@tabby.ai>
2024-02-02 19:00:08 +0200

    debug

--- v3/v3.go
+++ v3/v3.go
@@ -154,6 +154,7 @@ func (c *Client) CoinsOHLC(id string, days int) ([]types.OHLC, error) {
 	params.Add("vs_currency", "usd")
 	params.Add("days", strconv.Itoa(days))
 	url := fmt.Sprintf("%s/coins/%s/olhc?%s", baseURL, id, params.Encode())
+	fmt.Println(url)
 	resp, err := c.MakeReq(url)
 	if err != nil {
 		return nil, err
